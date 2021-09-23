a9c1bcfbba2b1073f85e0c193b00d93b51c68951
fouadissaad1 <71667992+fouadissaad1@users.noreply.github.com>
2021-09-23 16:24:09 +0200

    Style Contact add

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,37 +1,45 @@
 import React from "react";
+import { MDBCard, MDBCardText, MDBCardTitle, MDBContainer, MDBRow } from "mdbreact";
+
 
 export function Contact() {
     return (
-        <div>
-            <h2>Contact information</h2>
-            <p>You can contact clothing in various ways. Below is an overview of the different contact options.</p>
+        <MDBContainer>
+            <MDBRow>
+                <MDBCard className="card-body" style={{width: "21rem"}}>
+                    <MDBCardTitle><h2>Contact information</h2></MDBCardTitle>
+                    <MDBCardText>
+                        <p>You can contact clothing in various ways. Below is an overview of the different contact
+                            options.</p>
+                    </MDBCardText>
+                </MDBCard>
+            </MDBRow>
 
-        <div>
-            <h2>Consumer</h2>
-            <p>Question?</p>
-
-               <p>klantenservice@clothingshop.be</p>
-
-                <p>+32 (0) 493 99 99 44</p>
-
-               <p>Service and warranty</p>
-
-               <p> service@clothingshop.be</p>
-
-               <p>Payments and administrative</p>
-
-                <p>Question?</p>
-
-                <p>administratie@cothingshop.be</p>
-        </div>
-                <h2>For Business</h2>
-                <p>Question?</p>
-
-                <p> verkoop@cothingshop.be</p>
-
-                  <p>  +32 (0) 493 99 99 45</p>
-
-
-        </div>
+            <MDBRow>
+                    <MDBCard className="card-body" style={{width: "21rem"}}>
+                        <MDBCardTitle><h2>Consumer</h2></MDBCardTitle>
+                        <MDBCardText>
+                            <p>Question?</p>
+                            <p>klantenservice@clothingshop.be</p>
+                            <p>+32 (0) 493 99 99 44</p>
+                            <h4>Service and warranty</h4>
+                            <p> service@clothingshop.be</p>
+                            <h4 className="green-text">Payments and administrative</h4>
+                            <p>Question?</p>
+                            <p>administratie@cothingshop.be</p>
+                        </MDBCardText>
+                    </MDBCard>
+                <MDBCard className="card-body" style={{width: "22rem"}}>
+                    <MDBCardTitle><h2>For Business</h2></MDBCardTitle>
+                    <MDBCardText>
+                        <p>Question?</p>
+                        <p> verkoop@cothingshop.be</p>
+                        <p> +32 (0) 493 99 99 45</p>
+                    </MDBCardText>
+                </MDBCard>
+            </MDBRow>
+            <MDBRow>
+            </MDBRow>
+        </MDBContainer>
     );
 }
