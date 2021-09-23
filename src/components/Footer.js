5b7443270f9d3ef77d975939fc6dbc1deb624c43
fouadissaad1 <71667992+fouadissaad1@users.noreply.github.com>
2021-09-23 10:34:13 +0200

    install mdb and normalize css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,8 @@
-import React from "react";
 
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+import React from "react";
 function Footer(props){
     return<p className="pFooterStyle"> © 2021 Belgium Country Clothing • Fouad ISSAAD{props.name}</p>
 }
