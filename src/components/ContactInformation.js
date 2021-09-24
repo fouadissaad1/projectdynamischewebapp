a01f287787a227aa19759e8c1b218c228aa03ae9
fouadissaad1 <71667992+fouadissaad1@users.noreply.github.com>
2021-09-24 21:12:04 +0200

    add functions and refactor tot new components

---
+++ src/components/ContactInformation.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export function ContactInformation(props) {
+    return <p>You can contact clothing in various ways. Below is an overview of the different contact
+        options.</p>
+}
+
+export function Consumer(props) {
+    return <p>Question?<br></br>
+        klantenservice@clothingshop.be <br></br>
+        +32 (0) 493 99 99 44 <br></br>
+    </p>
+}
+
+export function ServiceWarranty(props) {
+    return <p> service@clothingshop.be</p>
+}
+
+export function Payments(props) {
+    return <p>Question? <br/>
+        administratie@cothingshop.be</p>
+}
+
+export function Business(props) {
+    return <p>Question?<br/>
+        verkoop@cothingshop.be<br/>
+        +32 (0) 493 99 99 45</p>
+}
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,6 @@
-import {MDBRow,MDBCol,MDBBtn,MDBCollapse} from "mdb-react-ui-kit";
+import { MDBBtn, MDBCol, MDBCollapse, MDBRow } from "mdb-react-ui-kit";
 import React, { useState } from "react";
-
+import { Business, Consumer, ContactInformation, Payments, ServiceWarranty } from "../components/ContactInformation";
 
 export function Contact() {
 
@@ -11,48 +11,40 @@ export function Contact() {
             <MDBRow></MDBRow>
             <MDBRow color={"red"} bgColor={"primary"}>
                 <MDBCol lg={1}></MDBCol>
-                <MDBCol lg={2} >
+                <MDBCol lg={2}>
                     <MDBBtn onClick={toggleShow}>Contact information</MDBBtn>
                     <MDBCollapse show={showShow}>
-                        <p>You can contact clothing in various ways. Below is an overview of the different contact
-                            options.</p>
+                        <ContactInformation></ContactInformation>
                     </MDBCollapse>
-            </MDBCol>
-            <MDBCol lg={2} >
-                <MDBBtn onClick={toggleShow}>Consumer</MDBBtn>
-                <MDBCollapse show={showShow}>
-                    <p>Question?</p>
-                    <p>klantenservice@clothingshop.be</p>
-                    <p>+32 (0) 493 99 99 44</p>
-                </MDBCollapse>
-            </MDBCol>
-                <MDBCol>
-                <MDBBtn onClick={toggleShow}>Service and warranty</MDBBtn>
-                <MDBCollapse show={showShow}>
-                    <p> service@clothingshop.be</p>
-                </MDBCollapse>
                 </MDBCol>
                 <MDBCol lg={2}>
-                <MDBBtn onClick={toggleShow}>Payments and administrative</MDBBtn>
-                <MDBCollapse show={showShow}>
-                    <p>Question?</p>
-                    <p>administratie@cothingshop.be</p>
-                </MDBCollapse>
-            </MDBCol>
-            <MDBCol lg={2} >
-                <MDBBtn onClick={toggleShow}>For Business</MDBBtn>
-                <MDBCollapse show={showShow}>
-                    <p>Question?</p>
-                    <p> verkoop@cothingshop.be</p>
-                    <p> +32 (0) 493 99 99 45</p>
-                </MDBCollapse>
+                    <MDBBtn onClick={toggleShow}>Consumer</MDBBtn>
+                    <MDBCollapse show={showShow}>
+                        <Consumer/>
+                    </MDBCollapse>
+                </MDBCol>
+                <MDBCol>
+                    <MDBBtn onClick={toggleShow}>Service and warranty</MDBBtn>
+                    <MDBCollapse show={showShow}>
+                        <ServiceWarranty/>
+                    </MDBCollapse>
+                </MDBCol>
+                <MDBCol lg={2}>
+                    <MDBBtn onClick={toggleShow}>Payments and administrative</MDBBtn>
+                    <MDBCollapse show={showShow}>
+                        <Payments/>
+                    </MDBCollapse>
+                </MDBCol>
+                <MDBCol lg={2}>
+                    <MDBBtn onClick={toggleShow}>For Business</MDBBtn>
+                    <MDBCollapse show={showShow}>
+                        <Business/>
+                    </MDBCollapse>
 
-            </MDBCol>
-            <MDBCol lg={1}></MDBCol>
-        </MDBRow>
+                </MDBCol>
+                <MDBCol lg={1}></MDBCol>
+            </MDBRow>
             <MDBRow></MDBRow>
-            </React.Fragment>
-
-
+        </React.Fragment>
     );
 }
