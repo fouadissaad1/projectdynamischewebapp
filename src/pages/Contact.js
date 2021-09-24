118e20d6aca8c110b1467cce80f55013e0bc95ec
fouadissaad1 <71667992+fouadissaad1@users.noreply.github.com>
2021-09-24 20:34:29 +0200

    Resolve issue wicht mdbbtn and style contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,45 +1,58 @@
-import React from "react";
-import { MDBCard, MDBCardText, MDBCardTitle, MDBCol, MDBContainer, MDBRow } from "mdbreact";
+import {MDBRow,MDBCol,MDBBtn,MDBCollapse} from "mdb-react-ui-kit";
+import React, { useState } from "react";
 
 
 export function Contact() {
+
+    const [showShow, setShowShow] = useState(false);
+    const toggleShow = () => setShowShow(!showShow);
     return (
-        <MDBContainer>
-              <MDBRow>
-                <MDBCard className="card-body" style={{width: "21rem"}}>
-                    <MDBCardTitle><h2>Contact information</h2></MDBCardTitle>
-                    <MDBCardText>
+        <React.Fragment>
+            <MDBRow></MDBRow>
+            <MDBRow color={"red"} bgColor={"primary"}>
+                <MDBCol lg={1}></MDBCol>
+                <MDBCol lg={2} >
+                    <MDBBtn onClick={toggleShow}>Contact information</MDBBtn>
+                    <MDBCollapse show={showShow}>
                         <p>You can contact clothing in various ways. Below is an overview of the different contact
                             options.</p>
-                    </MDBCardText>
-                </MDBCard>
-            </MDBRow>
+                    </MDBCollapse>
+            </MDBCol>
+            <MDBCol lg={2} >
+                <MDBBtn onClick={toggleShow}>Consumer</MDBBtn>
+                <MDBCollapse show={showShow}>
+                    <p>Question?</p>
+                    <p>klantenservice@clothingshop.be</p>
+                    <p>+32 (0) 493 99 99 44</p>
+                </MDBCollapse>
+            </MDBCol>
+                <MDBCol>
+                <MDBBtn onClick={toggleShow}>Service and warranty</MDBBtn>
+                <MDBCollapse show={showShow}>
+                    <p> service@clothingshop.be</p>
+                </MDBCollapse>
+                </MDBCol>
+                <MDBCol lg={2}>
+                <MDBBtn onClick={toggleShow}>Payments and administrative</MDBBtn>
+                <MDBCollapse show={showShow}>
+                    <p>Question?</p>
+                    <p>administratie@cothingshop.be</p>
+                </MDBCollapse>
+            </MDBCol>
+            <MDBCol lg={2} >
+                <MDBBtn onClick={toggleShow}>For Business</MDBBtn>
+                <MDBCollapse show={showShow}>
+                    <p>Question?</p>
+                    <p> verkoop@cothingshop.be</p>
+                    <p> +32 (0) 493 99 99 45</p>
+                </MDBCollapse>
 
-            <MDBRow>
-                    <MDBCard className="card-body" style={{width: "21rem"}}>
-                        <MDBCardTitle><h2>Consumer</h2></MDBCardTitle>
-                        <MDBCardText>
-                            <p>Question?</p>
-                            <p>klantenservice@clothingshop.be</p>
-                            <p>+32 (0) 493 99 99 44</p>
-                            <h4>Service and warranty</h4>
-                            <p> service@clothingshop.be</p>
-                            <h4 className="green-text">Payments and administrative</h4>
-                            <p>Question?</p>
-                            <p>administratie@cothingshop.be</p>
-                        </MDBCardText>
-                    </MDBCard>
-                <MDBCard className="card-body" style={{width: "22rem"}}>
-                    <MDBCardTitle><h2>For Business</h2></MDBCardTitle>
-                    <MDBCardText>
-                        <p>Question?</p>
-                        <p> verkoop@cothingshop.be</p>
-                        <p> +32 (0) 493 99 99 45</p>
-                    </MDBCardText>
-                </MDBCard>
-            </MDBRow>
-            <MDBRow>
-            </MDBRow>
-        </MDBContainer>
+            </MDBCol>
+            <MDBCol lg={1}></MDBCol>
+        </MDBRow>
+            <MDBRow></MDBRow>
+            </React.Fragment>
+
+
     );
 }
