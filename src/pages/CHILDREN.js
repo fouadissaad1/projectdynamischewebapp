661f600be4b972d1edfc413bff5f4367ed44387c
fouadissaad1 <71667992+fouadissaad1@users.noreply.github.com>
2021-10-02 16:53:20 +0200

    add database boy acc

--- src/pages/CHILDREN.js
+++ src/pages/CHILDREN.js
@@ -6,6 +6,12 @@ import KidAccBugsPink from "../images/clothting/kids/girls/accesoires/V1721A6_21
 import KidLingBugsBeige from "../images/clothting/kids/girls/Leggings/H6991A6_20AU_BG287_01_01_600x.jpg";
 import KidLingBugsGe from "../images/clothting/kids/girls/Leggings/T5991A6_20WN_GN7_01_01_600x.jpg";
 import KidLingBugsBe from "../images/clothting/kids/girls/Leggings/H6991A6_20AU_BR88_01_02_600x.jpg";
+import KidSkirtsBe from "../images/clothting/kids/girls/skirts&shorts/U7572A6_21SM_BG285_02_01_600x.jpg";
+import KidSkirtst from "../images/clothting/kids/girls/skirts&shorts/T3841A6_21SP_BK27_01_02_600x.jpg";
+import KidSkirtbr from "../images/clothting/kids/girls/skirts&shorts/S5917A6_20WN_BN101_01_02_600x.jpg";
+import KidAccBoy from "../images/clothting/kids/boy/acce/N6830A6_21SP_IN8_02_02_600x.jpg";
+import KidAccBoyHa from "../images/clothting/kids/boy/acce/S7000A6_21AU_BR2_01_01_600x.jpg";
+import KidAccBoyU from "../images/clothting/kids/boy/acce/U0056A6_20WN_BK27_02_02_600x.jpg";
 
 const Kids_data = [
     {
@@ -61,6 +67,60 @@ const Kids_data = [
                 image: <img src={KidLingBugsBe} alt="KidLingBugsBe"/>
             }
         ]
+    },
+    {
+        name:"Skirts & Shorts",
+        KidPruducts:[
+            {
+                id:1,
+                name:"Basic Drawstring Shorts",
+                color: "Beige",
+                price: 7.95,
+                image: <img src={KidSkirtsBe} alt="KidSkirtsBe"/>
+            },
+            {
+                id:2,
+                name:"Basic Shorts",
+                color: "Black",
+                price: 10.95,
+                image: <img src={KidSkirtst} alt="KidSkirtst"/>
+            },
+            {
+                id:3,
+                name:"Regular Fit Woven Skirt",
+                color: "Beige",
+                price: 25.95,
+                image: <img src={KidSkirtbr} alt="KidSkirtbr"/>
+            }
+
+        ]
+    },
+    {
+        title: "Boy",
+        name: "Accesoires",
+        KidPruducts: [
+            {
+                id:1,
+                name:"Cars' Printed Cap",
+                color: "indigo",
+                price: 8,
+                image: <img src={KidAccBoy} alt="KidAccBoy"/>
+            },
+            {
+                id:2,
+                name:"Harry Potter Licensed Backpack ",
+                color: "Bordeaux",
+                price: 14,
+                image: <img src={KidAccBoyHa} alt="KidAccBoyHa"/>
+            },
+            {
+                id:3,
+                name:"Licensed Discovery Channel Backpack ",
+                color: "Black",
+                price: 8,
+                image: <img src={KidAccBoyU} alt="KidAccBoyU"/>
+            }
+        ]
     }
 
 ]
@@ -80,7 +140,6 @@ function Kid(props) {
     const {kid} = props;
     return <div>
         <h3>{kid.name}</h3>
-        <p className="pMenStyle">size: {kid.size} </p>
         <p className="pMenStyle">color: {kid.color}</p>
         <p className="pMenStyle">how much: {kid.price}&euro;</p>
         {kid.image}
