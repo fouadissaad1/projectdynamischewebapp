fb95269c576ab713348a7ead52e6a0a1b296c690
fouadissaad1 <71667992+fouadissaad1@users.noreply.github.com>
2021-10-02 11:34:18 +0200

    layout MEN

--- src/pages/MEN.js
+++ src/pages/MEN.js
@@ -1,48 +1,64 @@
 import React from "react";
 import { MEN_CLOTHING_DATA } from "../data/data";
-import { MDBBtn, MDBCard, MDBCardBody, MDBCardText, MDBCardTitle } from "mdb-react-ui-kit";
+import {
+    MDBBtn,
+    MDBCard,
+    MDBCardBody,
+    MDBCardText,
+    MDBCardTitle,
+    MDBCol,
+    MDBContainer,
+    MDBRow
+} from "mdb-react-ui-kit";
 
-function Category(props){
-    const {category}=props;
+function Category(props) {
+    const {category} = props;
     return <div>
         <h3 className="title">{category.name}</h3>
         {
-            category.products.map((p)=><Man key={p.id} man={p}/>)
+            category.products.map((p) => <Man key={p.id} man={p}/>)
         }
     </div>
 }
 
-function Man(props){
-    const {man}=props;
+function Man(props) {
+    const {man} = props;
     return (
-        <MDBCard style={{ maxWidth: '22rem' }}>
-            {man.image}
-            <MDBCardBody>
-                <MDBCardTitle>{man.name}</MDBCardTitle>
-                <MDBCardText>
-                    <p className="pMenStyle">size: {man.size} </p>
-                    <p className="pMenStyle">color: {man.color}</p>
-                    <p className="pMenStyle">how much: {man.prijs}&euro;</p>
-
-                </MDBCardText>
-                <MDBBtn href='#'>Ordre</MDBBtn>
-            </MDBCardBody>
-        </MDBCard>
+        <MDBContainer>
+            <MDBRow>
+                <MDBCol>
+                    <MDBCard style={{maxWidth: '22rem'}}>
+                        {man.image}
+                        <MDBCardBody>
+                            <MDBCardTitle>{man.name}</MDBCardTitle>
+                            <MDBCardText>
+                                <p className="pMenStyle">size: {man.size} </p>
+                                <p className="pMenStyle">color: {man.color}</p>
+                                <p className="pMenStyle">how much: {man.prijs}&euro;</p>
+                            </MDBCardText>
+                            <MDBBtn href='#'>Ordre</MDBBtn>
+                        </MDBCardBody>
+                    </MDBCard>
+                </MDBCol>
+            </MDBRow>
+        </MDBContainer>
     );
 }
 
-
 export function MEN() {
     return (
-        <div>
-
-            <h2>Men</h2>
-
-            <hr/>
-            <div>
-                {MEN_CLOTHING_DATA.map((c,i)=><Category category={c} key={i}/>)}
-            </div>
-
-        </div>
+        <MDBContainer fluid>
+            <MDBRow>
+                <h2>Men</h2>
+                <hr/>
+            </MDBRow>
+            <MDBRow>
+                {MEN_CLOTHING_DATA.map((c, i) =>
+                    <MDBCol>
+                        <Category category={c} key={i}/>
+                    </MDBCol>
+                )}
+            </MDBRow>
+        </MDBContainer>
     );
 }
